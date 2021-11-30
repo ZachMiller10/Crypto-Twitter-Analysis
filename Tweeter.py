27bc4f0007fa5928af21bf6fc92292608719df66
John Ramsey Moss <89094838+Jmoss3041@users.noreply.github.com>
2021-11-29 18:28:47 -0600

    Actual Data

--- Tweeter.py
+++ Tweeter.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 
 ### Crypto Search
-c = twint.Config() #topic
+c = twint.Config() 
+c.Search = ['#crypto (since:2018-12-31 until:2021-04-20)']  #topic#topic
 c.Limit = 1000000  #number of tweets to scrape
 c.Store_csv = True  #store tweets to csv file
 c.Output = "crypto_hashtag_tweets.csv"  #path to csv file
@@ -19,7 +20,7 @@ df1 = pd.read_csv('crypto_hashtag_tweets.csv')
 
 ###Dogecoin Search
 d = twint.Config()
-d.Search = ['#dogecoin']  #topic
+d.Search = ['#dogecoin (since:2018-12-31 until:2021-12-31)']  #topic
 d.Limit = 1000000   #number of tweets to scrape
 d.Store_csv = True  #store tweets to csv file
 d.Output = "dogecoin_hashtag_tweets.csv"  #path to csv file
@@ -32,7 +33,7 @@ df2 = pd.read_csv('dogecoin_hastag_tweets.csv')
 
 ###Bitcoin search
 b = twint.Config()
-b.Search = ['#bitcoin']  #topic
+b.Search = ['#bitcoin (since:2018-12-31 until:2021-12-31)']  #topic
 b.Limit = 1000000   #number of tweets to scrape
 b.Store_csv = True  #store tweets to csv file
 b.Output = "bitcoin_hashtag_tweets.csv"  #path to csv file
@@ -46,7 +47,7 @@ df3 = pd.read_csv('bitcoin_hashtag_tweets.csv')
 ###@elonmusk search
 
 e = twint.Config()
-e.Search = ['(@elonmusk) until:2018-12-31 since:2021-12-31)']  #topic
+e.Search = ['@elonmusk (since:2018-12-31 until:2021-12-31)']  #topic
 e.Limit = 1000000   #number of tweets to scrape
 e.Store_csv = True  #store tweets to csv file
 e.Output = "elon_tweets.csv"  #path to csv file
@@ -60,7 +61,7 @@ df4 = pd.read_csv('elon_tweets.csv')
 ###Cryptocom
 
 f = twint.Config()
-f.Search = ['@cryptocom']  #topic
+f.Search = ['@cryptocom (since:2018-12-31 until:2021-12-31)']  #topic
 f.Limit = 1000000   #number of tweets to scrape
 f.Store_csv = True  #store tweets to csv file
 f.Output = "cryptocom_tweets.csv"  #path to csv file
